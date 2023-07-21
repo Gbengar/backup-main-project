aafa6d1223c6ddcb3c9a6bacbebee612cf0cc1bb
Gbenga Oshagbemi <oshagbemigbenga@gmail.com>
2023-07-21 18:08:20 +0100

    done

--- prod backend/server.js
+++ prod backend/server.js
@@ -26,12 +26,13 @@ const app = express();
 //   })
 // );`
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace this with your frontend URL
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow requests with credentials
   next();
 });
 
